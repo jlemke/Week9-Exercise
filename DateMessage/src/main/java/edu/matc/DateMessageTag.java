fb61962b2f9bd73512fc633fa6ca9cfa132c787c
jlemke <serikosx@gmail.com>
2017-05-09 23:26:45 -0500

    reviewed and modified multi-threading exercise

--- DateMessage/src/main/java/edu/matc/DateMessageTag.java
+++ DateMessage/src/main/java/edu/matc/DateMessageTag.java
@@ -32,9 +32,17 @@ public class DateMessageTag extends SimpleTagSupport {
         else if (hour >= 18)
             greeting = "Good evening!";
 
+        //check if its the last day of class 2017
+        if (calendar.get(Calendar.MONTH) == 4 && calendar.get(Calendar.DAY_OF_MONTH) == 9)
+            greeting += " It's the last day of class!";
+
         //check if its halloween
         if (calendar.get(Calendar.MONTH) == 9 && calendar.get(Calendar.DAY_OF_MONTH) == 31)
             greeting += " Happy Halloween!";
+
+        //check if its chrismas
+        if (calendar.get(Calendar.MONTH) == 11 && calendar.get(Calendar.DAY_OF_MONTH) == 25)
+            greeting += " Merry Christmas!";
 
         //print the message
         out.println(greeting);
